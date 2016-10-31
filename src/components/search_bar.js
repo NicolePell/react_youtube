828287307e2189928a8570b118ab6d0c7133cf67
NicolePell <pellicena@gmail.com>
2016-10-31 10:36:10 +0000

    L19: set search bar value from state making a controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,14 +4,16 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };
+    this.state = { term: 'Search youtube' };
   }
 
   render() {
     return (
       <div>
-        <input onChange={event => this.setState({ term: event.target.value })} />
-        Value of the input: {this.state.term}
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+          Value of the input: {this.state.term}
       </div>
     );
   }
