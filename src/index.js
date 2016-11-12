a3df3241ad7cae3064a50352e887d5d5d64067d5
NicolePell <pellicena@gmail.com>
2016-11-12 11:14:06 +0000

    L25: add api call to state and pass down props to video list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-// create component to produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+const YOUTUBE_API_KEY = 'AIzaSyD9hSS05MLEfBOn6wQx73pRqaiDV3CqKSo';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: YOUTUBE_API_KEY, term: 'pandas'}, (videos) => {
+      this.setState({ videos });
+      // es6 syntax where the strings are the same and will resolve to videos: videos
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-// take component's generated html and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
