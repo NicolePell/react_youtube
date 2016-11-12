31759cf97cbebb13c1cee3836f17db34c46fe958
NicolePell <pellicena@gmail.com>
2016-11-12 15:03:00 +0000

    L29: set selectedVideo when clicking on the video list item

--- src/index.js
+++ src/index.js
@@ -12,20 +12,27 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: YOUTUBE_API_KEY, term: 'pandas'}, (videos) => {
-      this.setState({ videos });
-      // es6 syntax where the strings are the same and will resolve to videos: videos
-    })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
   }
 
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
